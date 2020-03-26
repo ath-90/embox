dc6a15071d18cbbd3e00078143e3933dfcd467ea
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-03-26 15:15:13 +0300

    mips/subarch/p5600/mips32_p5600_arch.c

--- src/arch/mips/subarch/p5600/mips32_p5600_arch.c
+++ src/arch/mips/subarch/p5600/mips32_p5600_arch.c
@@ -4,37 +4,16 @@
  * @date Mar 20, 2020
  * @uthor Anton Bondarev
  */
+
+#include <util/log.h>
+
 #include <stdint.h>
 
 #include <asm/mipsregs.h>
 
 #include <hal/arch.h>
 
-extern void global_control_block_init(void);
-
 void arch_init(void) {
-	uint32_t status;
-	uint32_t cause;
-	uint32_t intctl;
-	uint32_t config3;
-
-	status = mips_read_c0_status();
-	if (status & ST0_BEV) {
-
-	}
-	cause = mips_read_c0_cause();
-	if (cause & CAUSE_IV) {
-
-	}
-	intctl = mips_read_c0_intctl();
-	if (intctl & INTCTL_VS){
-
-	}
-	config3 = mips_read_c0_congig3();
-	if (config3 & CONFIG3_VEXIC){
-		config3 = CONFIG3_VINT;
-	}
-
 }
 
 void arch_idle(void) {
